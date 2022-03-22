88e7c37f455669cf4dda63f66afe607748434be2
tcadwall <97193754+tcadwall@users.noreply.github.com>
2022-03-22 15:05:54 -0700

    First complete version

--- Packet6_scratchwwork.R
+++ Packet6_scratchwwork.R
@@ -7,7 +7,47 @@
 
 
 # install.packages("infer")
-library(infer)
+# library(infer)
+# 
+# data(gss)
+# table(gss$college)
 
-data(gss)
-table(gss$college)
+#[Overview of the `gssr` package](https://kjhealy.github.io/gssr/articles/overview.html)
+
+library(tidyverse)
+#install.packages("drat")
+library(drat)
+
+drat::addRepo("kjhealy")
+#install.packages("gssr")
+library(gssr)
+
+data(gss_doc)
+data(gss_all)
+gss_which_years(gss_all, grass)
+
+num_vars <- c("hrsrelax", "mntlhlth", "hrs1")
+cat_vars <- c("degree", "grass")
+my_vars <- c(num_vars, cat_vars)
+
+gss18 <- gss_get_yr(2018)
+gss18 <- gss18 %>% 
+  select(my_vars) %>% 
+  mutate(
+    # Convert all missing to NA
+    across(everything(), haven::zap_missing),
+    # Make all categorical variables factors and relabel nicely
+    across(all_of(cat_vars), forcats::as_factor))
+  
+
+gss10 <- gss_get_yr(2010)
+gss10 <- gss10 %>% 
+  select(my_vars) %>% 
+  mutate(
+    # Convert all missing to NA
+    across(everything(), haven::zap_missing),
+    # Make all categorical variables factors and relabel nicely
+    across(all_of(cat_vars), forcats::as_factor))
+
+table(gss18$grass) %>% prop.table()
+table(gss10$grass)
