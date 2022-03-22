479c60887a6a4a6bb342f344f504e32c9cecc482
tcadwall <97193754+tcadwall@users.noreply.github.com>
2022-03-22 15:12:59 -0700

    Allows docs folder

--- Packet6_scratchwwork.R
+++ Packet6_scratchwwork.R
@@ -15,11 +15,11 @@
 #[Overview of the `gssr` package](https://kjhealy.github.io/gssr/articles/overview.html)
 
 library(tidyverse)
-#install.packages("drat")
+install.packages("drat")
 library(drat)
 
 drat::addRepo("kjhealy")
-#install.packages("gssr")
+install.packages("gssr")
 library(gssr)
 
 data(gss_doc)
@@ -32,7 +32,7 @@ my_vars <- c(num_vars, cat_vars)
 
 gss18 <- gss_get_yr(2018)
 gss18 <- gss18 %>% 
-  select(my_vars) %>% 
+  select(all_of(my_vars)) %>% 
   mutate(
     # Convert all missing to NA
     across(everything(), haven::zap_missing),
@@ -42,7 +42,7 @@ gss18 <- gss18 %>%
 
 gss10 <- gss_get_yr(2010)
 gss10 <- gss10 %>% 
-  select(my_vars) %>% 
+  select(all_of(my_vars)) %>% 
   mutate(
     # Convert all missing to NA
     across(everything(), haven::zap_missing),
@@ -51,3 +51,64 @@ gss10 <- gss10 %>%
 
 table(gss18$grass) %>% prop.table()
 table(gss10$grass)
+
+
+num_vars <- c()
+cat_vars <- c("partyid", "grass")
+my_vars <- c(num_vars, cat_vars)
+
+party_grass18 <- gss_get_yr(2018)
+party_grass18 <- party_grass18 %>% 
+  select(all_of(my_vars)) %>% 
+  mutate(
+    # Convert all missing to NA
+    across(everything(), haven::zap_missing),
+    # Make all categorical variables factors and relabel nicely
+    across(all_of(cat_vars), forcats::as_factor))
+
+table(party_grass18)
+
+party_grass18 <- party_grass18 %>% 
+  mutate(
+    party = recode(partyid,
+                   "strong democrat" = "DEM",
+                   "not str democrat" = "DEM",
+                   "ind,near dem" = "IND",
+                   "independent" = "IND",
+                   "ind,near rep" = "IND",
+                   "not str republican" = "REP",
+                   "strong republican" = "REP",
+                   "other party" = "OTH"
+                   )
+  )
+
+table(party_grass18$party, party_grass18$grass) %>% prop.table(margin = 1)
+
+num_vars <- c()
+cat_vars <- c("degree", "grass")
+my_vars <- c(num_vars, cat_vars)
+
+my_gss18 <- gss_get_yr(2018)
+my_gss18 <- my_gss18 %>% 
+  select(all_of(my_vars)) %>% 
+  mutate(
+    # Convert all missing to NA
+    across(everything(), haven::zap_missing),
+    # Make all categorical variables factors and relabel nicely
+    across(all_of(cat_vars), forcats::as_factor))
+
+table(my_gss18)
+
+my_gss18 <- my_gss18 %>% 
+  mutate(
+    college = recode(degree,
+                   "lt high school" = "no degree",
+                   "high school" = "no degree",
+                   "junior college" = "degree",
+                   "bachelor" = "degree",
+                   "graduate" = "degree"
+    )
+  )
+
+table(my_gss18$college, my_gss18$grass) %>% addmargins()
+table(my_gss18$college, my_gss18$grass) %>% prop.table(margin = 1)
